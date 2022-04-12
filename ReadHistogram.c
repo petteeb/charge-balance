9b877d43a857d0038a2dd2f2f7ffe13bf6e78174
petteeb <bepettee@uncg.edu>
2022-04-11 21:32:27 -0400

    Added fit to "product".

--- ReadHistogram.c
+++ ReadHistogram.c
@@ -6,6 +6,13 @@ void ReadHistogram()
   TProfile* h_q1q2_deta = (TProfile*)f->Get("h_q1q2_deta");
   TProfile* h_q1_deta = (TProfile*)f->Get("h_q1_deta");
   TProfile* h_q2_deta = (TProfile*)f->Get("h_q2_deta");
+
+  double xmin;
+  double xmax;
+  
+  ifstream text("bounds.txt");
+  text >> xmin >> xmax;
+  text.close();
   
   cout << h_q1q2_deta << endl;
   cout << h_q1_deta << endl;
@@ -16,11 +23,22 @@ void ReadHistogram()
   cout << h1_q1q2_deta << endl;
   cout << h1_q1_deta << endl;
   cout << h1_q2_deta << endl;
+  TH1D* product = new TH1D();
+
+  *product = (*h1_q1_deta * *h1_q2_deta) - *h1_q1q2_deta;
+  product->Fit("gaus", "", "", xmin, xmax);  
+  product->SetTitle("Product");
+  product->Draw();
+  c1->Print("product.png");
   h1_q1q2_deta->Draw();
   c1->Print("1histnew.png");
   h1_q1_deta->Draw();
   c1->Print("2histnew.png");
   h1_q2_deta->Draw();
   c1->Print("3histnew.png");
- 
+  
+  //h1_q1_deta->Multiply(h1_q2_deta);
+  //h1_q1_deta->Add(h1_q1q2_deta, -1.0);
+  // h1_q1_deta->Draw();
+  //c1->Print("test.png");
 }
